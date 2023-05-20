54affdb4a1ae3263d39ac3e0ac8b242811fe2a5a
WhiteInfamous24 <juanmanuel.caceres24@outlook.com>
2023-05-19 23:52:09 -0300

    update

--- PIC-KeyboardDisplay7Seg.X/nbproject/Makefile-impl.mk
+++ PIC-KeyboardDisplay7Seg.X/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=PIC-Display7Seg.X
+PROJECTNAME=PIC-KeyboardDisplay7Seg.X
 
 # Active Configuration
 DEFAULTCONF=default
