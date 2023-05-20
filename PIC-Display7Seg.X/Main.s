676fbcc4e675ae05df4dd501170c6b36184dba7e
WhiteInfamous24 <juanmanuel.caceres24@outlook.com>
2023-05-19 23:45:40 -0300

    update

--- PIC-Display7Seg.X/Main.s
+++ PIC-Display7Seg.X/Main.s
@@ -30,7 +30,12 @@ INT_VECT:
     SWAPF   STATUS, W
     MOVWF   STATUS_TMP
     
-    ; IMPLEMENT METHOD INTERRUPTION
+    ; keypad routine
+    CALL    getKeypad
+    BANKSEL PORTB
+    MOVLW   0b00000000		; put pins RB4 to RB7 in /HIGH
+    MOVWF   PORTB
+    BCF	    INTCON, 1		; clear INTF flag
     
     ; return previous context
     SWAPF   STATUS_TMP, W
@@ -43,61 +48,154 @@ INT_VECT:
 ; program variables
 W_TMP	    EQU 0x20
 STATUS_TMP  EQU	0x21
-CTER_0	    EQU 0X22
-CTER_1	    EQU 0X23
-CTER_2	    EQU 0X24
+KYPD_BTN    EQU 0x22
+KYPD_FND_F  EQU	0x23
 
 ; program setup
 setup:
     
-    ; memory bank selection
-    BSF	    STATUS, 5		; set bit 5 of STATUS vector, to select the memory bank 1 (01)
+    ; PORTC configuration
+    BANKSEL TRISC
+    MOVLW   0b00000000		; set PORTC pins as output
+    MOVWF   TRISC
     
     ; PORTB configuration
-    MOVLW   0xFF
-    MOVWF   TRISB		; set lower nibble bits of TRISB vector, to put all the pin in output mode
+    BANKSEL TRISB
+    MOVLW   0b00001111		; set 4 outputs and 4 inputs to control 8x8 keyboard in PORTB
+    MOVWF   TRISB
+    BANKSEL ANSELH
+    CLRF    ANSELH		; set PORTB as digital inputs
     
-    ; memory bank selection
-    BCF	    STATUS, 5		; clear bit 5 of STATUS vector, to select the memory bank 0 (00)
+    ; general port configuration
+    BANKSEL OPTION_REG		; enable global pull-ups
+    MOVLW   0b00000000		; | /RBPU | INTEDG | T0CS | T0SE | PSA | PS2 | PS1 | PS0 |
+    MOVWF   OPTION_REG
+    BANKSEL WPUB
+    MOVLW   0b00001111		; enable RB0 to RB3 pull-ups
+    MOVWF   WPUB
+    
+    ; interruption configuration
+    BANKSEL INTCON		; enable global interruptions and interruptions in PORTB
+    MOVLW   0b10010000		; | GIE | PEIE | T0IE | INTE | RBIE | T0IF | INTF | RBIF |
+    MOVWF   INTCON
+    BANKSEL IOCB
+    MOVLW   0b00001111		; enable interruptions in pins RB0 to RB3
+    MOVWF   IOCB
+    
+    ; PORTC initialization
+    BANKSEL PORTC
+    MOVLW   0b00000000
+    MOVWF   PORTC
+    
+    ; PORTB initialization
+    BANKSEL PORTB
+    MOVLW   0b00000000		; put pins RB4 to RB7 in /HIGH
+    MOVWF   PORTB
+    
+    ; variables initialization
+    CLRF    KYPD_BTN
+    CLRF    KYPD_FND_F
 
 ; main program loop
 main:
-    MOVLW   0x02		; load in W the value that i want to search in sevSegDeco
-    CALL    sevSegDeco
-    MOVWF   PORTB		; set the bits of PORTB vector with the values returned from the sevSegDeco
-    CALL    delay
+    
+    ; show last pressed button
+    CALL    showButton
     
     GOTO    main
-
-sevSegDeco:
-    ADDWF   PCL, 1
-    RETLW   0x00
-    RETLW   0x01
-    RETLW   0x02
-    RETLW   0x03
-    RETLW   0x04
-    RETLW   0x05
-    RETLW   0x06
-    RETLW   0x07
-    RETLW   0x08
-    RETLW   0x09
-
-delay:
-    MOVLW   0xFF
-    MOVWF   CTER_0
-loop_2:
-    MOVLW   0xFF
-    MOVWF   CTER_1
-loop_1:
-    MOVLW   0xFF
-    MOVWF   CTER_2
-loop_0:
-    DECFSZ  CTER_2
-    GOTO    loop_0
-    DECFSZ  CTER_1
-    GOTO    loop_1
-    DECFSZ  CTER_0
-    GOTO    loop_2
+    
+; show the pressed button subroutine
+showButton:
+    BANKSEL PORTC
+    MOVF    KYPD_BTN, W
+    MOVWF   PORTC
+    
     RETURN
+
+; get pressed button in keypad subroutine
+getKeypad:
+    CLRF    KYPD_FND_F
+    BANKSEL PORTB
+    
+    BSF	    PORTB, 7
+    BSF	    PORTB, 6
+    BSF	    PORTB, 5
+    BCF	    PORTB, 4
+    MOVLW   0b00010000
+    MOVWF   KYPD_BTN
+    CALL    getRow
+    BTFSC   KYPD_FND_F, 0
+    
+    RETURN
+    
+    BSF	    PORTB, 7
+    BSF	    PORTB, 6
+    BCF	    PORTB, 5
+    BSF	    PORTB, 4
+    MOVLW   0b00100000
+    MOVWF   KYPD_BTN
+    CALL    getRow
+    BTFSC   KYPD_FND_F, 0
+    
+    RETURN
+    
+    BSF	    PORTB, 7
+    BCF	    PORTB, 6
+    BSF	    PORTB, 5
+    BSF	    PORTB, 4
+    MOVLW   0b01000000
+    MOVWF   KYPD_BTN
+    CALL    getRow
+    BTFSC   KYPD_FND_F, 0
+    
+    RETURN
+    
+    BCF	    PORTB, 7
+    BSF	    PORTB, 6
+    BSF	    PORTB, 5
+    BSF	    PORTB, 4
+    MOVLW   0b10000000
+    MOVWF   KYPD_BTN
+    CALL    getRow
+    BTFSC   KYPD_FND_F, 0
+    
+    RETURN
+    
+; get row of pressed button subroutine
+getRow:
+    BTFSC   PORTB, 0
+    GOTO    setRow_0
+    BTFSC   PORTB, 1
+    GOTO    setRow_1
+    BTFSC   PORTB, 2
+    GOTO    setRow_2
+    BTFSC   PORTB, 3
+    GOTO    setRow_3
+    
+    RETURN
+    
+setRow_0:
+    BSF	    KYPD_BTN, 0
+    BSF	    KYPD_FND_F, 0
+    
+    RETURN
+    
+setRow_1:
+    BSF	    KYPD_BTN, 1
+    BSF	    KYPD_FND_F, 0
+    
+    RETURN
+    
+setRow_2:
+    BSF	    KYPD_BTN, 2
+    BSF	    KYPD_FND_F, 0
+    
+    RETURN
+    
+setRow_3:
+    BSF	    KYPD_BTN, 3
+    BSF	    KYPD_FND_F, 0
+    
+    RETURN    
 
 END RESET_VECT
